01730713ad0698cad28dd821680950016f512e96
Jonas B. Halvorsen <jbh93hoved.annet@gmail.com>
2021-10-01 12:20:06 +0200

    navigation finished. Bottom navigation bar added.

--- app/src/main/java/com/example/visualplanner/MainActivity.java
+++ app/src/main/java/com/example/visualplanner/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.visualplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment)
+                getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
+        assert navHostFragment != null;
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_deadlines, R.id.navigation_events).build();
+
+        NavController controller = navHostFragment.getNavController();
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        NavigationUI.setupWithNavController(navView, controller);
+
+        NavigationUI.setupActionBarWithNavController(this, controller, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, controller);
+
     }
 }
---
+++ app/src/main/java/com/example/visualplanner/ui/deadlines/DeadlinesFragment.java
@@ -0,0 +1,25 @@
+package com.example.visualplanner.ui.deadlines;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.visualplanner.R;
+
+public class DeadlinesFragment extends Fragment {
+
+    public DeadlinesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_deadlines, container, false);
+    }
+}
