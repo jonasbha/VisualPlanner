0501ede2d2d6a2f99ef206a638a35f0ab300e9ed
Jonas B. Halvorsen <jbh93hoved.annet@gmail.com>
2021-10-04 10:18:41 +0200

    replaced toolbar with imagebutton. Moved button from MainActivity to DeadlinesFragment. Styled the button.

--- app/src/main/java/com/example/visualplanner/ui/deadlines/DeadlinesFragment.java
+++ app/src/main/java/com/example/visualplanner/ui/deadlines/DeadlinesFragment.java
@@ -2,24 +2,39 @@ package com.example.visualplanner.ui.deadlines;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.ui.AppBarConfiguration;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.visualplanner.MainActivity;
 import com.example.visualplanner.R;
+
+import java.util.Objects;
 
 public class DeadlinesFragment extends Fragment {
 
-    public DeadlinesFragment() {
-        // Required empty public constructor
-    }
+    public DeadlinesFragment() {}
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_deadlines, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button drawerBtn = view.findViewById(R.id.openDrawerBtn);
+        drawerBtn.setOnClickListener(v -> ((MainActivity) requireActivity()).openDrawer());
     }
 }
