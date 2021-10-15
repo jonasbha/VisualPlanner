8ea0cf450a65291eba6ae49a59e3d2f721f6390f
Jonas B. Halvorsen <jbh93hoved.annet@gmail.com>
2021-10-15 13:55:28 +0200

    small changes. committing before adding database module

---
+++ app/src/main/java/com/example/visualplanner/repository/IEventRepository.java
@@ -0,0 +1,4 @@
+package com.example.visualplanner.repository;
+
+public interface IEventRepository {
+}
--- app/src/main/java/com/example/visualplanner/ui/event/AddEventActivity.java
+++ app/src/main/java/com/example/visualplanner/ui/event/AddEventActivity.java
@@ -1,16 +1,25 @@
 package com.example.visualplanner.ui.event;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.visualplanner.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddEventActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+
+        myRef.setValue("Hello World");
     }
+
+
 }
