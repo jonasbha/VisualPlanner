eb0b87a346b57e82c2e5f9360508186eb449adb2
Jonas B. Halvorsen <jbh93hoved.annet@gmail.com>
2021-10-29 10:05:50 +0200

    added databinding to EventRecycleAdapter

--- app/src/main/java/com/example/visualplanner/adapter/EventRecycleAdapter.java
+++ app/src/main/java/com/example/visualplanner/adapter/EventRecycleAdapter.java
@@ -3,16 +3,12 @@ package com.example.visualplanner.adapter;
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.visualplanner.R;
+import com.example.visualplanner.databinding.EventBinding;
 import com.example.visualplanner.model.Event;
 
 import java.util.List;
@@ -42,9 +38,9 @@ public class EventRecycleAdapter extends RecyclerView.Adapter<EventRecycleAdapte
     @Override
     public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Log.d("EventRecyclerAdapter", "Creating View");
-        View view = inflater.inflate(R.layout.event, parent, false);
+        EventBinding eventBinding = EventBinding.inflate(inflater, parent, false);
 
-        return new EventViewHolder(view);
+        return new EventViewHolder(eventBinding);
     }
 
     @Override
@@ -60,38 +56,27 @@ public class EventRecycleAdapter extends RecyclerView.Adapter<EventRecycleAdapte
     }
 
     public class EventViewHolder extends RecyclerView.ViewHolder {
-        TextView eventTitle;
-        TextView timeInput;
-        TextView dateInput;
-        TextView locationInput;
-        ImageView alarmIcon;
-        ImageView locationIcon;
-        ImageView calendarIcon;
-        ImageButton deleteIcon;
 
-        public EventViewHolder(@NonNull View itemView) {
-            super(itemView);
-            eventTitle = itemView.findViewById(R.id.eventTitle);
-            timeInput = itemView.findViewById(R.id.timeInput);
-            dateInput = itemView.findViewById(R.id.dateInput);
-            locationInput = itemView.findViewById(R.id.locationInput);
-            alarmIcon = itemView.findViewById(R.id.alarmIcon);
-            locationIcon = itemView.findViewById(R.id.locationIcon);
-            calendarIcon = itemView.findViewById(R.id.calendarIcon);
-            deleteIcon = itemView.findViewById(R.id.deleteIcon);
+        private final EventBinding binding;
 
-            deleteIcon.setOnClickListener(v -> {
-                if (clickListener != null) {
-                    int position = getAdapterPosition();
-                    if (position != RecyclerView.NO_POSITION) {
-                        clickListener.onDeleteClick(position);
-                    }
+
+        public EventViewHolder(EventBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void onDelete() {
+            if (clickListener != null) {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+                    clickListener.onDeleteClick(position);
                 }
-            });
+            }
         }
 
         public void bind(Event currentEvent) {
-            eventTitle.setText(currentEvent.getTitle());
+            binding.setView(this);
+            binding.setEvent(currentEvent);
         }
     }
 }
