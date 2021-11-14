9f533616161670d2cd28891aa0cd2384e57689df
Jonas B. Halvorsen <jbh93hoved.annet@gmail.com>
2021-11-14 11:42:28 +0100

    timeleft rendering (not live)

--- app/src/main/java/com/example/visualplanner/utility/Format.java
+++ app/src/main/java/com/example/visualplanner/utility/Format.java
@@ -1,5 +1,10 @@
 package com.example.visualplanner.utility;
 
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+import android.util.Log;
+
 public class Format {
 
     public static String date(String datetime) {
@@ -14,4 +19,26 @@ public class Format {
             return "kl. ".concat(datetime.substring(11, 16));
         return null;
     }
+
+    public static String daysLeft(long timeLeft) {
+        timeLeft = timeLeft - Calendar.getInstance().getTimeInMillis();
+        long daysLeft = TimeUnit.MILLISECONDS.toDays(timeLeft);
+        long hoursLeft = TimeUnit.MILLISECONDS.toHours(timeLeft);
+        long minutesLeft = TimeUnit.MILLISECONDS.toMinutes(timeLeft);
+
+        if (timeLeft < 0)
+            return "Hendelse ferdig.";
+
+        if (minutesLeft < 15)
+            return "Straks!";
+
+        if (hoursLeft < 24 && hoursLeft > 1)
+            return "Innen " + hoursLeft + " timer.";
+
+        if (daysLeft == 1)
+            return daysLeft + " dag igjen.";
+        else
+            return daysLeft + " dager igjen.";
+    }
+
 }
